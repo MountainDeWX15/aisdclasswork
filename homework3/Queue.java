dfb12777d22b081283680ec435052aa066c24685
Danil Shayakhmetov <141357248+MountainDeWX15@users.noreply.github.com>
2025-06-10 23:02:35 +0300

    Add files via upload

---
+++ homework3/Queue.java
@@ -0,0 +1,55 @@
+package Classwork;
+
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+public class Queue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public Queue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void add(int x) {
+        stack1.push(x);
+    }
+
+    public int remove() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    public boolean isEmpty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+    public int size() {
+        return stack1.size() + stack2.size();
+    }
+
+}
+
+
